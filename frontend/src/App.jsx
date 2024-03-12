56fa2a95a6f8346a5e57d45634f0295407f20a41
Dylan Luc <nguyd.luc@gmail.com>
2024-03-12 15:01:24 -0600

    updated App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,3 @@
-import { useEffect, useState } from "react";
-
-function App() {
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    async function fetchData() {
-      console.log(import.meta.env.VITE_API_URL);
-      try {
-        const response = await fetch(`${import.meta.env.VITE_API_URL}`);
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        const result = await response.json();
-        console.log(result);
-        setData(result);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    }
-    fetchData();
-  }, []);
-
-  return <div>{JSON.stringify(data)}</div>;
-}
+function App() {}
 
 export default App;
